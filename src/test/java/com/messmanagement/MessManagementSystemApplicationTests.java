180ab7e404144464f69b6c0fcd502b1847e849ea
deepak <piomega01@gmail.com>
2025-05-29 15:18:41 +0530

    Initial file setup

---
+++ src/test/java/com/messmanagement/MessManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.messmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
